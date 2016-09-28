5096e7acddf85d6c7ecdd6e3128475d6fb1e27da
Levi Saturnino <levisaturnino@gmail.com>
2016-09-28 00:55:58 -0300

    layout primeface

--- src/main/java/br/com/agentemaps/controller/AgenteController.java
+++ src/main/java/br/com/agentemaps/controller/AgenteController.java
@@ -1,5 +1,61 @@
 package br.com.agentemaps.controller;
 
-public class AgenteController {
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+import br.com.agentemaps.dao.AgenteDAO;
+import br.com.agentemaps.dao.AgenteDAOImpl;
+import br.com.agentemaps.model.Agente;
+@ManagedBean
+@SessionScoped
+public class AgenteController {
+	private Agente agente;
+	private DataModel<Agente> listaAgentes;
+
+	public DataModel<Agente> getListarAgentes() {
+		List<Agente> lista = new AgenteDAOImpl().listar();
+		listaAgentes = new ListDataModel<Agente>(lista);
+		return listaAgentes;
+	}
+
+	public Agente getAgente() {
+		return agente;
+	}
+
+	public void setAgente(Agente Agente) {
+		this.agente = Agente;
+	}
+
+	public String prepararAdicionarAgente() {
+		agente = new Agente();
+		return "04cadastrarAgente";
+	}
+
+	public String prepararAlterarAgente() {
+		agente = (Agente) (listaAgentes.getRowData());
+		return "04cadastrarAgente";
+	}
+
+	public String excluirAgente() {
+		Agente AgenteTemp = (Agente) (listaAgentes.getRowData());
+		AgenteDAO dao = new AgenteDAOImpl();
+		dao.excluir(AgenteTemp);
+		return "listaagente";
+	}
+
+	public String adicionarAgente() {
+		AgenteDAOImpl dao = new AgenteDAOImpl();
+		dao.salvar(agente);
+		return "listaagente";
+	}
+
+	public String alterarAgente() {
+		AgenteDAO dao = new AgenteDAOImpl();
+		dao.atualizar(agente);
+		return "listaagente";
+	}
 }
--- src/main/java/br/com/agentemaps/dao/AgenteDAO.java
+++ src/main/java/br/com/agentemaps/dao/AgenteDAO.java
@@ -2,16 +2,19 @@ package br.com.agentemaps.dao;
 
 import java.util.List;
 
+import br.com.agentemaps.model.Administrador;
 import br.com.agentemaps.model.Agente;
 
 public interface AgenteDAO {
 	
-	public void salvar(Agente agente);
+	public void salvar(Agente administrador);
 
 	public List<Agente> listar();
 
-	public void atualizar(Agente agente);
+	public void atualizar(Agente administrador);
 
-	public void excluir(Agente agente);
+	public void excluir(Agente administrador);
+	
+	public Agente getAgente(long id);
 
 }
